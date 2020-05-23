25b00f0fffc6519573b5cd3b88fc194e9f7bb403
Charlie <charlie@falcons.dev>
2020-05-23 02:32:43 +0100

    added a bunch of schemas for serialisation

--- backend/helpr/database.py
+++ backend/helpr/database.py
@@ -1,7 +1,7 @@
 """
 Holds marshmallow schemas and database models for the application.
 """
-from helpr import db
+from helpr import db, ma
 
 posting_categories = db.Table('posting_categories',
                               db.Column('posting_id', db.Integer, db.ForeignKey('posting.id'), primary_key=True),
@@ -20,6 +20,11 @@ class Posting(db.Model):
                            backref=db.backref('posting', lazy=True))
 
 
+class PostingSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = Posting
+
+
 class Business(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200))
@@ -28,10 +33,20 @@ class Business(db.Model):
     postings = db.relationship('Posting', backref='poster', lazy=True)
 
 
+class BusinessScehma(ma.SQLAlchemySchema):
+    class Meta:
+        model = Business
+
+
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     description = db.Column(db.String(500))
+
+
+class CategorySchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Category
 
 
 class User(db.Model):
@@ -44,7 +59,17 @@ class User(db.Model):
     service_requests = db.relationship('ServiceRequests', backref='requester', lazy=True)
 
 
+class UserSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = User
+
+
 class ServiceRequests(db.Model):
     request_id = db.Column(db.Integer, primary_key=True)
     user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     details = db.Column(db.String(1000))
+
+
+class ServiceRequestSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = ServiceRequests
