fb0bd8067a62b4f174bcb3c751fb11db7bc117cd
Charlie <charlie@falcons.dev>
2020-05-23 16:03:23 +0100

    added some authy routes

--- backend/helpr/auth.py
+++ backend/helpr/auth.py
@@ -1,6 +1,7 @@
 """
 This is where all authentication related routes belong.
 """
+from flask import g, request, abort
 
 from helpr import app, db, auth
 from helpr.database import User
@@ -17,3 +18,25 @@ def verify_password(username_or_token, password):
             return False
     g.user = user
     return True
+
+
+@app.route('/account/token')
+@auth.login_required
+def get_auth_token():
+    token = g.user.generate_auth_token(600)
+    return {'token': token.decode('ascii'), 'duration': 600}
+
+
+@app.route('/account/users', methods=['POST'])
+def new_user():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        abort(400)  # missing arguments
+    if User.query.filter_by(username=username).first() is not None:
+        abort(400)  # existing user
+    user = User(username=username)
+    user.hash_password(password)
+    db.session.add(user)
+    db.session.commit()
+    return {'username': user.username}, 201
